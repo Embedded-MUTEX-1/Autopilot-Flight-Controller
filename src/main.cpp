074c2699c215d8ade8969faf91e5618117834e54
lenny1411 <105748537+lenny1411@users.noreply.github.com>
2024-01-12 21:07:48 +0100

    Modify project config and test debug

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,16 @@
 #include <Arduino.h>
+#include "utils/utils.h"
+
+#define LED_BUILTIN 2
 
 void setup() {
     // TODO Init and start tasks
+    pinMode(LED_BUILTIN, OUTPUT);
 }
 
 void loop() {
-    // TODO disable this task
+    digitalWrite(LED_BUILTIN, HIGH);  // turn the LED on (HIGH is the voltage level)
+    delay_milis(100); 
+    digitalWrite(LED_BUILTIN, LOW);   // turn the LED off by making the voltage LOW
+    delay_milis(100);   
 }
